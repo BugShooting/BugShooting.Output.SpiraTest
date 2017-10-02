698dc9e1291b5fa178ed7f5bb0c068782fb1fd2f
Alexej Hirsch <alexej@bugshooting.com>
2017-10-02 21:56:25 +0200

    Projektdateien hinzufügen.

---
+++ BS.Output.SpiraTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BS.Output.SpiraTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bug Shooting")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3E4D2240-B32D-463D-AA31-988521C1FF61")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
